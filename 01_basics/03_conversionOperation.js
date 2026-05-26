ff96f378ece052fd579a579b5200a281a84b7e9e
Rautpawan <149668086+Rautpawan@users.noreply.github.com>
2026-05-26 09:30:30 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33aa" // null // undefined;
+
+console.log(typeof(score));
+
+let value = Number(score);
+console.log(typeof value);
+console.log(value)  // NaN
+
+// "33" => 33
+// "234cdsdd" => NaN
+// true => 1  // false=>0;
+
+let islogged = 1;
+let boolLogged = Boolean(islogged);
+console.log(typeof boolLogged);
+console.log(boolLogged);
+
+
