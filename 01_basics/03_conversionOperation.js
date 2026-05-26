80842ab1e2a7747397b74af372b5e3e509cd5b8b
Rautpawan <149668086+Rautpawan@users.noreply.github.com>
2026-05-26 09:54:46 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,18 +1,43 @@
-let score = "33aa" // null // undefined;
+// let score = "33aa" // null // undefined;
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let value = Number(score);
-console.log(typeof value);
-console.log(value)  // NaN
+// let value = Number(score);
+// console.log(typeof value);
+// console.log(value)  // NaN
 
-// "33" => 33
-// "234cdsdd" => NaN
-// true => 1  // false=>0;
+// // "33" => 33
+// // "234cdsdd" => NaN
+// // true => 1  // false=>0;
 
-let islogged = 1;
-let boolLogged = Boolean(islogged);
-console.log(typeof boolLogged);
-console.log(boolLogged);
+// let islogged = 1;
+// let boolLogged = Boolean(islogged);
+// console.log(typeof boolLogged);
+// console.log(boolLogged);
 
 
+//      Operations // 
+
+let value = 1;
+let negativeValue = -value;
+console.log(negativeValue);
+
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3/2)
+console.log(3%2)
+console.log(3**2)
+
+let str1 = "Hello"
+let str2 = " World"
+console.log(str1+str2);
+
+console.log("1"+2)  // =>12
+console.log(1+"2"); // =>12
+console.log("1"+2+2); // =>122
+console.log(1+2+"2"); // =>32
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
