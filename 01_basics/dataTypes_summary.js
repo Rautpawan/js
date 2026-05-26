da31d364e6908710ba649059d89b1fb2068e33bd
Rautpawan <149668086+Rautpawan@users.noreply.github.com>
2026-05-26 11:13:45 +0000

    primitive and non primitive datatypes

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,25 @@
+
+// Primitive => String, Number, Boolean, null, undefined, Symbol, Bigint
+
+const name = "USer"
+const value = 33.33;
+const Iscorrect = true;
+const Temperature = null;
+let userId;
+const id = Symbol('123');
+const bigNumber = 15648522584652153n;
+
+
+// Non primitive => Array, Objects, Functions
+
+const heros = ["shaktiman", "Hanuman", "Spiderman"]
+
+const myObj = {
+    name:"user",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+myFunction();
